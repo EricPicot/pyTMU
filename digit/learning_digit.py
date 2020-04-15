a38d6a9589a483a75f7884bdacdb741b69846e4c
EricPicot <35737562+EricPicot@users.noreply.github.com>
2020-04-15 15:38:53 +0200

    Update learning_digit.py

--- digit/learning_digit.py
+++ digit/learning_digit.py
@@ -1,9 +1,7 @@
 import numpy as np
-import tensorflow as tf
-from tensorflow.keras import datasets, layers, models
-import matplotlib.pyplot as plt
 import tf2_processing
 from build_models import digit_model
+
 def transform_target(target):
     target_list = []
     for t in target:
@@ -17,15 +15,7 @@ data = tf2_processing.read_image("./digit/speed_digit_data/digit_data.npy")
 target = np.load("./digit/speed_digit_data/digit_target.npy").astype(int)
 target = transform_target(target).astype('float32').reshape((-1,10))
 
-# model = digit_model()
-#
-#
-# print(target.shape)
-# model.compile(optimizer="adam", loss="categorical_crossentropy")
-# model.fit(data, target, epochs=50, verbose=1)
-# model.save('./models/digit_model')
-
-model = tf.keras.models.load_model('./models/digit_model')
-d = data[0]
-print(d.shape)
-print(model.predict(np.reshape(d, (1,30,20,1))))
+model = digit_model()
+model.compile(optimizer="adam", loss="categorical_crossentropy")
+model.fit(data, target, epochs=50, verbose=1)
+model.save('./models/digit_model')
